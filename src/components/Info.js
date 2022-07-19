01e6eebc8713591a859b04cb9a640099652e1768
tep yith <tep.yith@gmail.com>
2022-07-18 21:32:43 -0400

    Change info section, added marquee

--- src/components/Info.js
+++ src/components/Info.js
@@ -36,57 +36,30 @@ const Info = ({backgroundColor}) => {
         animate='visible' 
         exit='exit'  
       >
-        <section className='info'>
-          <div className='row'>
-            <div className="column center">
-              <h3>Skills</h3>
-            </div>
-            <div className='column'>
-              <div className='carousel'>
-                <div className='change-outer'>
-                  <div className='change-inner'>
-                    <div className='element'><h4>HTML</h4></div>
-                    <div className='element'><h4>CSS / SCSS</h4></div>
-                    <div className='element'><h4>Javascript</h4></div>
-                    <div className='element'><h4>ReactJS & Redux</h4></div>
-                    <div className='element'><h4>And many more!</h4></div>
-                  </div>
-                </div>
-              </div>
+        <section className='info center'>
+          <div className='marquee'>
+            <div className='marquee-inner'>
+              <span><span className='info-subject'>Skills</span> HTML • Javascript • CSS • SASS • ReactJS • Figma • And many more!</span>
+              <span><span className='info-subject'>Skills</span> HTML • Javascript • CSS • SASS • ReactJS • Figma • And many more!</span>
+              <span><span className='info-subject'>Skills</span> HTML • Javascript • CSS • SASS • ReactJS • Figma • And many more!</span>
+              <span><span className='info-subject'>Skills</span> HTML • Javascript • CSS • SASS • ReactJS • Figma • And many more!</span>
             </div>
           </div>
         </section>
         <section className='info'>
-          <div className='row'>
-            <div className='column center'>
-              <h3>Other Likes</h3>
-            </div>
-            <div className='column left'>
-              <div className='container-other'>
-                <div className='change-other'>
-                  <h4 className="other-likes">Dogs, hockey, and potatoes</h4>
-                </div>
-              </div>
-            </div>
+          <div className='marquee'>
+            <span><span className='info-subject'>Other likes</span>Dogs, hockey, and potatoes</span>
           </div>
         </section>
         <section className='info'>
-          <div className='row'>
-            <div className='column center'>
-             <h3>To Know More</h3>
-            </div>
-            <div className='column left'>
-              <div className='container-other'>
-                <div className='change-other'>
-                  <div className='icons'>
-                    <a href='https://www.linkedin.com/in/tep-y/' target='_blank'><i className="fa-brands fa-linkedin"></i></a>
-                    <a href='https://github.com/tep-y' target='_blank'><i className="fa-brands fa-github"></i></a>
-                    <a href='https://www.instagram.com/robauheart/' target='_blank'><i className="fa-brands fa-instagram"></i></a>
-                    <a href='mailto:tep.yith@gmail.com' target='_blank'><i className="fa-solid fa-square-envelope"></i></a>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div className='marquee'>
+            <span>
+              <span className='info-subject'>To know more</span>
+                <a href='https://www.linkedin.com/in/tep-y/' target='_blank'><i className="fa-brands fa-linkedin"></i></a>
+                <a href='https://github.com/tep-y' target='_blank'><i className="fa-brands fa-github"></i></a>
+                <a href='https://www.instagram.com/robauheart/' target='_blank'><i className="fa-brands fa-instagram"></i></a>
+                <a href='mailto:tep.yith@gmail.com' target='_blank'><i className="fa-solid fa-square-envelope"></i></a>
+            </span>
           </div>
         </section>
       </motion.div>
